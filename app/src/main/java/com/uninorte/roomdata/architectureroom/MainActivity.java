2f276a5940cbbe77753fd5af6d78085a74ba6ec5
ErwinBustillo <erwinb1996@gmail.com>
2018-03-12 11:28:20 -0500

    added edit functionality

--- app/src/main/java/com/uninorte/roomdata/architectureroom/MainActivity.java
+++ app/src/main/java/com/uninorte/roomdata/architectureroom/MainActivity.java
@@ -88,4 +88,10 @@ public class MainActivity extends AppCompatActivity {
     public void onClickAdd(View view) {
         model.addUser(new User(data.size()+1,"Erwin"+(data.size()+1),"Bustillo"+(data.size()+1)));
     }
+
+    public void onClickEdit(View view) {
+        User user = data.get(0);
+        user.setFirstName(user.getFirstName()+"changed");
+        model.addUser(user);
+    }
 }
